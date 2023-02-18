9e82b9ae56d776c50d7d762f1d5b119f2a223132
Michael Barati <101454502+INeddHelp@users.noreply.github.com>
2023-02-18 14:30:27 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+
+intents = discord.Intents.default()
+intents.members = True  # Enable the privileged members intent
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('Ready')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if client.user.mentioned_in(message):
+        await message.channel.send('Hello!')
+
+client.run("your-token")
